6ce9d1fac31ae5fee646f58ab7a58232a67a9063
Wil St. Charles <wils@qti.qualcomm.com>
2016-09-19 14:57:12 -0600

    Adding transcode selection preference for HDTC-2US.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -32,11 +32,11 @@ def Start():
     VideoClipObject.thumb = R(ICON)
 
 @handler(PREFIX, TITLE, thumb=ICON)
- 
+
 def MainMenu():
     global pyhdhr
     pyhdhr = PyHDHR()
-    
+
     oc = ObjectContainer()
     oc.add(DirectoryObject(key=Callback(ShowLiveTV, title=L('LiveTV')), title=L('LiveTV'), thumb=R(ICON_LIVETV)))
     oc.add(DirectoryObject(key=Callback(ShowMainRecordingsMenu, title=L('Recordings')), title=L('Recordings'), thumb=R(ICON_RECORDINGS)))
@@ -63,7 +63,7 @@ def ShowLiveTV(title):
     oc.add(DirectoryObject(key=Callback(ShowFavoriteChannels, title=L('FavoriteChannels')), title=L('FavoriteChannels'), thumb=R(ICON_FAVORITES)))
     oc.add(DirectoryObject(key=Callback(ShowAllChannels, title=L('AllChannels')), title=L('AllChannels'), thumb=R(ICON_ALLCHANNELS)))
     oc.add(DirectoryObject(key=Callback(ShowHDChannels, title=L('HDChannels')), title=L('HDChannels'), thumb=R(ICON_HDCHANNELS)))
-    oc.add(DirectoryObject(key=Callback(ShowSDChannels, title=L('SDChannels')), title=L('SDChannels'), thumb=R(ICON_SDCHANNELS)))        
+    oc.add(DirectoryObject(key=Callback(ShowSDChannels, title=L('SDChannels')), title=L('SDChannels'), thumb=R(ICON_SDCHANNELS)))
     oc.add(DirectoryObject(key=Callback(ShowWhatsOn, title=L('WhatsOn')), title=L('WhatsOn'), thumb=R(ICON_LIVETV)))
     oc.add(InputDirectoryObject(key=Callback(SearchLiveTV), title=L('SearchLiveTV'), thumb=R(ICON_SEARCH)))
     return oc
@@ -72,10 +72,10 @@ def ShowLiveTV(title):
 def ShowFavoriteChannels(title):
     global pyhdhr
     oc = ObjectContainer(title2=title)
-    
+
     if not pyhdhr:
         pyhdhr = PyHDHR()
-        
+
     cl = pyhdhr.getChannelList()
     for line in cl:
         chan = pyhdhr.getChannelInfo(str(line))
@@ -87,10 +87,10 @@ def ShowFavoriteChannels(title):
 def ShowAllChannels(title):
     global pyhdhr
     oc = ObjectContainer(title2=title)
-    
+
     if not pyhdhr:
         pyhdhr = PyHDHR()
-        
+
     cl = pyhdhr.getChannelList()
     for line in cl:
         chan = pyhdhr.getChannelInfo(str(line))
@@ -101,25 +101,25 @@ def ShowAllChannels(title):
 def ShowHDChannels(title):
     global pyhdhr
     oc = ObjectContainer(title2=title)
-    
+
     if not pyhdhr:
         pyhdhr = PyHDHR()
-        
+
     cl = pyhdhr.getChannelList()
     for line in cl:
         chan = pyhdhr.getChannelInfo(str(line))
         if chan.getHD() == 1:
             oc.add(DirectoryObject(key=Callback(ShowTunedTVStaged,title=str(line),guideno=str(line)), title=str(line),thumb=(chan.getImageURL() if chan.getImageURL() else R(ICON_BROKEN))))
     return oc
-    
+
 @route(PREFIX + '/showsdchannels')
 def ShowSDChannels(title):
     global pyhdhr
     oc = ObjectContainer(title2=title)
-    
+
     if not pyhdhr:
         pyhdhr = PyHDHR()
-        
+
     cl = pyhdhr.getChannelList()
     for line in cl:
         chan = pyhdhr.getChannelInfo(str(line))
@@ -131,15 +131,15 @@ def ShowSDChannels(title):
 def ShowWhatsOn(title):
     global pyhdhr
     oc = ObjectContainer(title2=title)
-    
+
     if not pyhdhr:
         pyhdhr = PyHDHR()
-    
+
     progs = pyhdhr.getWhatsOn()
     for guideno in progs:
         oc.add(DirectoryObject(key=Callback(ShowTunedTVStaged, title=str(progs[guideno].getTitle() + " - " + progs[guideno].getEpisodeTitle()),guideno=guideno), title=str(progs[guideno].getTitle() + " - " + progs[guideno].getEpisodeTitle()), thumb=(progs[guideno].getImageURL() if progs[guideno].getImageURL() else R(ICON_UNKNOWN))))
     return oc
-    
+
 @route(PREFIX + '/showtunedtvstaged')
 def ShowTunedTVStaged(title,guideno):
     oc = ObjectContainer(title2=title)
@@ -158,10 +158,10 @@ def ShowMainRecordingsMenu(title):
 def ShowAllRecordings(title):
     global pyhdhr
     oc = ObjectContainer(title2=title)
-    
+
     if not pyhdhr:
         pyhdhr = PyHDHR()
-        
+
     rp = pyhdhr.getFilteredRecordedPrograms(SortType.asc,GroupType.All,None)
     if rp:
         for r in rp:
@@ -169,17 +169,17 @@ def ShowAllRecordings(title):
             if obj:
                 oc.add(obj)
     return oc
-    
+
 @route(PREFIX + '/showrecordingsbyseriesmenu')
 def ShowRecordingsBySeriesMenu(title):
     global pyhdhr
     oc = ObjectContainer(title2=title)
-    
+
     if not pyhdhr:
         pyhdhr = PyHDHR()
 
     series = pyhdhr.getRecordedSeries()
-    
+
     if series:
         for titlekey in sorted(series):
             oc.add(TVShowObject(key=Callback(ShowRecordingsBySeries, title=str(titlekey), seriesid=series[titlekey].getSeriesID()), title=str(titlekey), thumb=(series[titlekey].getImageURL() if series[titlekey].getImageURL() else R(ICON_UNKNOWN)),episode_count=(series[titlekey].getEpisodeCount()-1),rating_key=series[titlekey].getSeriesID()))
@@ -189,7 +189,7 @@ def ShowRecordingsBySeriesMenu(title):
 def ShowRecordingsBySeries(title,seriesid):
     global pyhdhr
     oc = ObjectContainer(title2=title)
-    
+
     if not pyhdhr:
         pyhdhr = PyHDHR()
 
@@ -205,7 +205,7 @@ def ShowRecordingsBySeries(title,seriesid):
 def SearchAll(query):
     global pyhdhr
     oc = ObjectContainer(title2="search all...")
-    
+
     if not pyhdhr:
         pyhdhr = PyHDHR()
 
@@ -219,7 +219,7 @@ def SearchAll(query):
 
     if len(progs) > 0:
         for recprogkey in progs:
-            oc.add(ShowRecording(recprogkey=recprogkey))    
+            oc.add(ShowRecording(recprogkey=recprogkey))
     oc.objects.sort(key = lambda obj: obj.title)
     return oc
 
@@ -227,7 +227,7 @@ def SearchAll(query):
 def SearchLiveTV(query):
     global pyhdhr
     oc = ObjectContainer(title2="search live tv...")
-    
+
     if not pyhdhr:
         pyhdhr = PyHDHR()
 
@@ -242,7 +242,7 @@ def SearchLiveTV(query):
 def SearchRecorded(query):
     global pyhdhr
     oc = ObjectContainer(title2='Search Results for \''+str(query)+'\'')
-    
+
     if not pyhdhr:
         pyhdhr = PyHDHR()
 
@@ -250,7 +250,7 @@ def SearchRecorded(query):
 
     if len(progs) > 0:
         for recprogkey in progs:
-            oc.add(ShowRecording(recprogkey=recprogkey))    
+            oc.add(ShowRecording(recprogkey=recprogkey))
     return oc
 
 @route(PREFIX + '/showtunedtv')
@@ -258,32 +258,32 @@ def ShowTunedTV(guideno,include_container=False):
     global pyhdhr
     if not pyhdhr:
         pyhdhr = PyHDHR()
-    
+
     chaninfo = pyhdhr.getLiveTVChannelInfo(guideno)
     if chaninfo:
         liveurl = chaninfo.getURL()
         cont = 'mpegts'
         vcodec = 'mpeg2video'
         acodec = 'ac3'
-        
+
         t = chaninfo.getTuner()
         Log.Debug("Tuner: " + t.getModelNumber())
-        
+
         if t.getModelNumber() == "HDTC-2US":
-            if Prefs['UseDefaultTranscoding'] == True:
+            if Prefs['TranscodeType'] == "default":
                 transcodeoption = t.getTranscodeOption()
                 if not transcodeoption:
-                    liveurl = liveurl + "?transcode=none"
+                    transcodeoption = "none"
                     Log.Debug("Could not get tuner transcode option, using none")
-                else:
-                    Log.Debug("Tuner transcode option: " + transcodeoption)
-                    if transcodeoption != "none":
-                        cont = 'mp4'
-                        vcodec = 'h264'
-                        acodec = 'ac3'
-                    liveurl = liveurl + "?transcode="+transcodeoption
             else:
-                liveurl = liveurl + "?transcode=none"
+                transcodeoption = Prefs['TranscodeType']
+
+            Log.Debug("Tuner transcode option: " + transcodeoption)
+            if transcodeoption != "none":
+                cont = 'mp4'
+                vcodec = 'h264'
+                acodec = 'ac3'
+            liveurl = liveurl + "?transcode="+transcodeoption
         else:
             if chaninfo.getVideoCodec() == "MPEG2":
                 cont = 'mpegts'
@@ -302,12 +302,12 @@ def ShowTunedTV(guideno,include_container=False):
             else:
                 msg = "Unknown audio codec: " + chaninfo.getAudioCodec()+". Using default"
                 Log.Critical(msg)
-        
+
         p_title = ""
         p_stitle = ""
         p_synopsis = ""
         p_imageurl = ""
-        
+
         proginfo = pyhdhr.getWhatsOn(guideno)
         if not proginfo:
             p_title=chaninfo.getGuideNumber()
@@ -319,11 +319,11 @@ def ShowTunedTV(guideno,include_container=False):
             p_stitle=proginfo.getEpisodeTitle()
             p_synopsis=proginfo.getSynopsis()
             p_imageurl=proginfo.getImageURL()
-        
+
         brate = None
         aratio = None
         vpix = None
-        
+
         if chaninfo.getHD() == 1:
             aratio = "1.78"
             vpix = 1080
@@ -338,9 +338,9 @@ def ShowTunedTV(guideno,include_container=False):
                 brate = 4000
             else:
                 brate = 1500
-            
+
         Log.Debug("Using the following MediaObject profile:\ncontainer: "+cont+",\nvideo codec: "+vcodec+",\naudio codec: "+acodec+",\nbitrate: "+str(brate)+",\naspect_ratio: "+aratio+",\nvideo_resolution: "+str(vpix))
-        
+
         obj = EpisodeObject(
             key=Callback(ShowTunedTV,guideno=guideno,include_container=True),
             url=liveurl,
@@ -349,7 +349,7 @@ def ShowTunedTV(guideno,include_container=False):
             summary=p_synopsis,
             duration=14400000,
             thumb=(p_imageurl if p_imageurl else R(ICON_UNKNOWN)),
-            items = [   
+            items = [
                 MediaObject(
                     parts = [
                         PartObject(
@@ -364,9 +364,9 @@ def ShowTunedTV(guideno,include_container=False):
                     aspect_ratio=aratio,
                     video_resolution=vpix
                 )
-            ]  
+            ]
         )
-          
+
         if include_container:
             return ObjectContainer(objects=[obj])
         else:
@@ -381,29 +381,29 @@ def ShowRecording(recprogkey,include_container=False):
     global pyhdhr
     if not pyhdhr:
         pyhdhr = PyHDHR()
-    
+
     prog = pyhdhr.getRecordedProgram(recprogkey)
     if prog:
-        
+
         cont = 'mpegts'
         vcodec = 'mpeg2video'
         acodec = 'ac3'
         brate = 13000
         aratio = "1.78"
         vpix = 1080
-        
+
         chaninfo = pyhdhr.getChannelInfo(prog.getChannelNumber())
         if chaninfo:
             t = chaninfo.getTuner()
             Log.Debug("Tuner: " + t.getModelNumber())
-            
+
             if t.getModelNumber() == "HDTC-2US":
                 transcodeoption = t.getTranscodeOption()
                 if not transcodeoption:
                     Log.Debug("Could not get tuner transcode option, using none")
                     vcodec = 'mpeg2video'
                     acodec = 'ac3'
-                else:                
+                else:
                     Log.Debug("Tuner transcode option: " + transcodeoption)
                     if transcodeoption == "none":
                         cont = 'mpegts'
@@ -431,7 +431,7 @@ def ShowRecording(recprogkey,include_container=False):
                 else:
                     msg = "Unknown audio codec: " + chaninfo.getAudioCodec()+". Using default"
                     Log.Critical(msg)
-            
+
             if chaninfo.getHD() == 1:
                 aratio = "1.78"
                 vpix = 1080
@@ -451,7 +451,7 @@ def ShowRecording(recprogkey,include_container=False):
             Log.Critical(msg)
 
         Log.Debug("Using the following MediaObject profile:\ncontainer: "+cont+",\nvideo codec: "+vcodec+",\naudio codec: "+acodec+",\nbitrate: "+str(brate)+",\naspect_ratio: "+aratio+",\nvideo_resolution: "+str(vpix))
-                
+
         obj = EpisodeObject(
             key=Callback(ShowRecording,recprogkey=recprogkey,include_container=True),
             url=prog.getPlayURL(),
@@ -460,7 +460,7 @@ def ShowRecording(recprogkey,include_container=False):
             summary=prog.getSynopsis(),
             duration=((prog.getRecordEndTime() - prog.getRecordStartTime()) * 1000),
             thumb=(prog.getImageURL() if prog.getImageURL() else R(ICON_UNKNOWN)),
-            items = [   
+            items = [
                 MediaObject(
                     parts = [
                         PartObject(
@@ -475,9 +475,9 @@ def ShowRecording(recprogkey,include_container=False):
                     aspect_ratio=aratio,
                     video_resolution=vpix
                 )
-            ]  
+            ]
         )
-          
+
         if include_container:
             return ObjectContainer(objects=[obj])
         else:
